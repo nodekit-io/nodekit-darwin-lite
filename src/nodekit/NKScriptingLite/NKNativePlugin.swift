a9e6d7d130c317d1a3bbd39d9d8977fbd4cbeb06
Patrick Goley <patrick.goley@gmail.com>
2018-06-07 16:33:16 -0500

    add documentation for NKNativePlugin and NKNativeProxy

--- src/nodekit/NKScriptingLite/NKNativePlugin.swift
+++ src/nodekit/NKScriptingLite/NKNativePlugin.swift
@@ -6,9 +6,27 @@
 //  Copyright © 2018 OffGrid Networks. All rights reserved.
 //
 
-public protocol NKNativePlugin: AnyObject {
+import JavaScriptCore
+
+/*
+ A native object that will be registered under global[namespace] and
+ callable from JavaScript.
+ Options can include a JavaScript source file to be loaded like so:
+ ["js": "path/to/file.js" as NSString]
+ */
+@objc public protocol NKNativePlugin: AnyObject {
     
     var namespace: String { get }
     
     var options: [String: AnyObject] { get }
+}
+
+/*
+ A native object that can proxy native method calls to it's cooresponding
+ JSValue. This property will be nil after the NKScriptContext is disposed
+ to break any retain cycles.
+ */
+@objc public protocol NKNativeProxy: AnyObject {
+    
+    var nkScriptObject: JSValue? { get set }
 }
