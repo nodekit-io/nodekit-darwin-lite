36bcec7a3cd7a6ff55051709b72819fd06bcb27f
Patrick Goley <patrick.goley@gmail.com>
2019-01-31 13:03:21 -0600

    fixed warnings about unsafeBitCast

--- src/nodekit/NKScripting/util/NKArchive/NKArchive.swift
+++ src/nodekit/NKScripting/util/NKArchive/NKArchive.swift
@@ -46,7 +46,7 @@ open class NKArchive {
             
             else { return nil }
         
-        let bytes = unsafeBitCast((data as NSData).bytes, to: UnsafePointer<UInt8>.self)
+        let bytes = (data as NSData).bytes.assumingMemoryBound(to: UInt8.self)
         
         let len = data.count
         
@@ -64,7 +64,7 @@ open class NKArchive {
     
     public static func createFromData(_ path: String, data: Data) -> NKArchive? {
         
-        let bytes = unsafeBitCast((data as NSData).bytes, to: UnsafePointer<UInt8>.self)
+        let bytes = (data as NSData).bytes.assumingMemoryBound(to: UInt8.self)
         
         let len = data.count
         
@@ -118,7 +118,7 @@ public extension NKArchive {
         
         file.closeFile()
         
-        let bytes = unsafeBitCast((data as NSData).bytes, to: UnsafePointer<UInt8>.self)
+        let bytes = (data as NSData).bytes.assumingMemoryBound(to: UInt8.self)
         
         return NKAR_Uncompressor.uncompressWithFileBytes(_cdir, fromBytes: bytes)
     }
